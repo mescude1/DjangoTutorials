57769dd2490e6f66c630c4c75d5d0f76e955b96b
Mauricio Escudero <ae_mescudero@contractor.indeed.com>
2025-07-30 00:11:23 -0500

    wip

--- helloworld/pages/urls.py
+++ helloworld/pages/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import homePageView
+from .views import homePageView, AboutPageView, ProductShowView, ProductIndexView, ProductCreateView
 
 urlpatters = [
-   path("", homePageView, name='home')
+   path("", homePageView, name='home'),
+   path('about/', AboutPageView.as_view(), name='about'),
+   path('products/', ProductIndexView.as_view(), name='index'),
+   path('products/<str:id>', ProductShowView.as_view(), name='show'),
+   path('products/create', ProductCreateView.as_view(), name='form'),
 ]
--- helloworld/pages/views.py
+++ helloworld/pages/views.py
@@ -1,6 +1,98 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.forms import forms, CharField, FloatField
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.views import View
+from django.views.generic import TemplateView
+
+
 # Create your views here.
 
-def homePageView(request):
-    return HttpResponse('Hello World')
+class homePageView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context.update({
+            "title": "About us - Online Store",
+            "subtitle": "About us",
+            "description": "This is an about page ...",
+            "author": "Developed by: Mauricio Escudero Restrepo",
+        })
+        return context
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context.update({
+            "email": "lmario1@eafit.edu.co",
+            "phone": "+5731955555555",
+            "address": "123 evergreen st"
+        })
+        return context
+
+
+class Product:
+    products = [
+        {"id": "1", "name": "TV", "description": "Best TV"},
+        {"id": "2", "name": "iPhone", "description": "Best iPhone"},
+        {"id": "3", "name": "Chromecast", "description": "Best Chromecast"},
+        {"id": "4", "name": "Glasses", "description": "Best Glasses"}
+    ]
+
+
+class ProductIndexView(View):
+    template_name = 'products/index.html'
+
+    def get(self, request):
+        viewData = {}
+        viewData["title"] = "Products - Online Store"
+        viewData["subtitle"] = "List of products"
+        viewData["products"] = Product.products
+        return render(request, self.template_name, viewData)
+
+
+class ProductShowView(View):
+    template_name = 'products/show.html'
+
+    def get(self, request, id):
+        viewData = {}
+        product = Product.products[int(id) - 1]
+        if not product:
+            return HttpResponseRedirect("/home")
+        viewData["title"] = product["name"] + " - Online Store"
+        viewData["subtitle"] = product["name"] + " - Product information"
+        viewData["product"] = product
+        return render(request, self.template_name, viewData)
+
+
+class ProductForm(forms.Form):
+    name = CharField(required=True)
+    price = FloatField(required=True)
+
+
+class ProductCreateView(View):
+    template_name = 'products/create.html'
+
+
+    def get(self, request):
+        form = ProductForm()
+        viewData = {}
+        viewData["title"] = "Create product"
+        viewData["form"] = form
+        return render(request, self.template_name, viewData)
+
+
+    def post(self, request):
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            return redirect(form)
+        else:
+            viewData = {}
+            viewData["title"] = "Create product"
+            viewData["form"] = form
+            return render(request, self.template_name, viewData)
